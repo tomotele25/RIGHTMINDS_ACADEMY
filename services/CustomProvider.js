db631688f308f029317d23a6cb5c050df00d2cec
tomotele <tomotelechristopher25@gmail.com>
2025-04-23 17:03:31 +0100

    tweak backend url logic

--- services/CustomProvider.js
+++ services/CustomProvider.js
@@ -1,7 +1,17 @@
 import CredentialsProvider from "next-auth/providers/credentials";
 import axios from "axios";
 
-const BACKENDURL = process.env.NEXT_PUBLIC_BACKEND_DOMAIN;
+let BACKENDURL;
+
+if (process.env.NEXT_PUBLIC_BACKEND_DOMAIN) {
+  BACKENDURL = process.env.NEXT_PUBLIC_BACKEND_DOMAIN;
+} else {
+  if (process.env.NODE_ENV === "production") {
+    BACKENDURL = "https://rightmindsbackend.vercel.app";
+  } else {
+    BACKENDURL = "http://localhost:5000";
+  }
+}
 
 const CustomProvider = CredentialsProvider({
   name: "customProvider",
